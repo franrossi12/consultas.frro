a0ed52b806166a946781a54f4b5432763de62ae2
Unknown <rossifrancisco12@gmail.com>
2019-12-14 09:39:38 +0000

    Footer + fix servideprovider

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,18 +1,27 @@
 <footer class="section footer-classic context-dark bg-image" style="background: #171717;">
-        <div class="row ">
-            <div class="col-sm-12 col-md-6 col-xl-5 text-left">
-                <div class="container">
-                    <img src="{{asset('img/logoutnwhite.png')}}" class="footer-logo" alt="logo-utn">
-                    <br>
-                    <p>FACULTAD REGIONAL ROSARIO </p>
-                    <p>   Universidad Tecnológica Nacional</p>
-                    <p>   CONTACTOS: ZEBALLOS 1341 - S2000BQA - ROSARIO</p>
-                    <p>   0341 - 4481871   Teléfonos directos e Internos </p>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-12 text-left">
+                <ul>
                     @if (!Auth::check())
                     @else
-                    <a href="{{ mapsiteRoute() }}">Mapa del Sitio</a>
+                    <li><a href="{{ mapsiteRoute() }}">Mapa del Sitio</a></li>
                     @endif
-                </div>
+                    <li><a href="{{ route('soporte.index') }}">Formulario de Contacto</a></li>
+                </ul>
             </div>
+        </div>
+        <hr style="background: white">
+        <div class="row text-left">
+                    <div class="col-md-6 col-lg-6 col-12">
+                    <img src="{{asset('img/logoutnwhite.png')}}" class="footer-logo" alt="logo-utn">
+                    </div>
+                    <br>
+
+                    <div class="col-md-6 col-lg-6 col-12">
+                    <p>FACULTAD REGIONAL ROSARIO - Universidad Tecnológica Nacional</p>
+                    <p>   CONTACTOS: ZEBALLOS 1341 - S2000BQA - ROSARIO</p>
+                    <p>   0341 - 4481871   Teléfonos directos e Internos </p> 
+                    </div>
+                   
         </div>
 </footer>
